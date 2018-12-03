771992d943b0d28339079ee4570ab87e1812a450
mshigo <mikeshigo@gmail.com>
2018-12-02 22:45:59 -0800

    minor formatting adjustments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,32 +11,32 @@ $(document).on("click", "#done", function() {
 //questions array
 var questionsArr = [
     {
-        question: "True or false? Tottenham Hotspur Football Club has never won the Premier League.",
+        question: "#1 True or False? Tottenham Hotspur Football Club has never won the Premier League.",
         answers: ["True", "False"],
         correctAnswer: "True"
     },
     {
-        question: "True or false? Manchester United is the most decorated club in the Premier League with 13 League Titles.",
+        question: "#2 True or False? Manchester United is the most decorated club in the Premier League with 13 League Titles.",
         answers: ["True", "False"],
         correctAnswer: "True"
     },
     {
-        question: "True or false? The Premier League consists of 18 teams.",
+        question: "#3 True or False? The Premier League consists of 18 teams.",
         answers: ["True", "False"],
         correctAnswer: "False"
     },
     {
-        question: "True or false? The Premier League utilizes VAR (Video Assistant Referee) to review and possibly reverse calls.",
+        question: "#4 True or False? The Premier League utilizes VAR (Video Assistant Referee) to review and possibly reverse calls.",
         answers: ["True", "False"],
         correctAnswer: "False"
     },
     {
-        question: "True or false? The 3 teams with the least points at the end of the season are relegated to the division below.",
+        question: "#5 True or False? The 3 teams with the least points at the end of the season are relegated to the division below.",
         answers: ["True", "False"],
         correctAnswer: "True"
     },
     {
-        question: "True or false? Teams earn 3 points for a win, 1 point for a draw, and 0 points for a loss.",
+        question: "#6 True or False? Teams earn 3 points for a win, 1 point for a draw, and 0 points for a loss.",
         answers: ["True", "False"],
         correctAnswer: "True"
     }
